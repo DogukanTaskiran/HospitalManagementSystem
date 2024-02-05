c00fe93052647862682868434ceab00d1c00b61e
DogukanTaskiran <105669729+DogukanTaskiran@users.noreply.github.com>
2024-02-05 20:25:12 +0300

    Limitation

--- Entities/DTOs/ApplicationUserDTO.cs
+++ Entities/DTOs/ApplicationUserDTO.cs
@@ -4,12 +4,10 @@ namespace Entities.DTOs{
 	public class ApplicationUserDTO
 	{
 		[Required(ErrorMessage = "Name is required")]
-        //[RegularExpression(@"^[a-zA-ZğüşıöçĞÜŞİÖÇ -']+$", ErrorMessage = "Invalid character. Use only letters, spaces, dashes and single quotes.")]   // gerek de yokmuş zaten kendiliğinden her şeyi ekleyebiliyoruz
         [MaxLength(100, ErrorMessage = "Name cannot be longer than 100 characters")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Surname is required")]
-        //[RegularExpression(@"^[a-zA-ZğüşıöçĞÜŞİÖÇ -']+$", ErrorMessage = "Invalid character. Use only letters, spaces, dashes and single quotes.")]     // gerek de yokmuş zaten kendiliğinden her şeyi ekleyebiliyoruz
         [MaxLength(100, ErrorMessage = "Surname cannot be longer than 100 characters")]
         public string Surname { get; set; }
 
@@ -18,21 +16,20 @@ namespace Entities.DTOs{
         [MaxLength(15, ErrorMessage = "Phone number cannot be longer than 15 characters")]
 		public string PhoneNumber { get; set; }
 
-        [Required(ErrorMessage = "Age number is required")]
+        [Required(ErrorMessage = "Age is required")]
         [Range(0, 120, ErrorMessage = "Invalid age")]
         public int Age { get; set; }
 
-        [Required(ErrorMessage = "Height number is required")]
+        [Required(ErrorMessage = "Height is required (cm)")]
         [Range(0, 250, ErrorMessage = "Invalid height")]
         public int Height { get; set; }
 
-        [Required(ErrorMessage = "Weight number is required")]
+        [Required(ErrorMessage = "Weight is required (kg)")]
         [Range(0, 500, ErrorMessage = "Invalid weight")]
         public int Weight { get; set; }
 
 
         [Required(ErrorMessage = "Address is required")]
-        //[RegularExpression(@"^[a-zA-Z0-9ğüşıöçĞÜŞİÖÇğüşıöçİĞ\s\S\w\W ]*$")].   // gerek de yokmuş zaten kendiliğinden her şeyi ekleyebiliyoruz
         [MaxLength(200, ErrorMessage = "Address cannot be longer than 200 characters")]
         public string Address { get; set; }
 
@@ -52,7 +49,7 @@ namespace Entities.DTOs{
         
         [Required(ErrorMessage = "Password is required")]
 		[DataType(DataType.Password)]
-        //[RegularExpression(@"^(?=.*[a-zğüşıöç])(?=.*[A-ZĞÜŞİÖÇ])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$",         // şimdilik kolaylık olsun diye yorum satırı
+        //[RegularExpression(@"^(?=.*[a-zğüşıöç])(?=.*[A-ZĞÜŞİÖÇ])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$", 
         //ErrorMessage = "Password must have at least 1 lowercase letter, 1 uppercase letter, 1 digit, 1 special character and 8 characters in total.")]
         public string Password { get; set; }
 
