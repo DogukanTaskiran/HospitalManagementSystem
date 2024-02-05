70208c1e099679e63e546cb7c00cdaa96e6ca0de
DogukanTaskiran <105669729+DogukanTaskiran@users.noreply.github.com>
2024-02-05 20:12:30 +0300

    Register limitations

--- Controllers/PatientController.cs
+++ Controllers/PatientController.cs
@@ -11,6 +11,7 @@ using System.Security.Claims;
 
 namespace Hospital.Controllers
 {
+    [Authorize(Policy ="PatientPolicy")]
     public class PatientController : Controller
     {
 
@@ -112,6 +113,21 @@ namespace Hospital.Controllers
 
 
         }
+        public IActionResult Profile()
+        {
+            var userEmail = User.FindFirstValue(ClaimTypes.Name);
+            var Patient = _context.patients.ToList();
+            ApplicationUser patient = _context.patients
+            .Include(x => x.Appointments)
+            .SingleOrDefault(p => p.ApplicationUser.Email == userEmail);
+
+             if (patient == null)
+                {
+               return NotFound();
+                }
+
+             return View(patient);
+        }
 
         [HttpPost]
         public IActionResult AddAppointment(int doctorId, DateTime appointmentDate, int appointmentTime)
@@ -197,20 +213,6 @@ namespace Hospital.Controllers
         //     return View();
         // }
 
-        // public IActionResult Profile()
-        // {
-        //     var userEmail = User.FindFirstValue(ClaimTypes.Name);
-        //     // var Patient = _context.patients.ToList();
-        //     ApplicationUser patient = _context.patients
-        //     .Include(x => x.Appointments)
-        //     .SingleOrDefault(p => p.ApplicationUser.Email == userEmail);
-
-        //     if (patient == null)
-        //     {
-        //         return NotFound();
-        //     }
-
-        //     return View(patient);
-        // }
+       
     }
 }
--- Entities/DTOs/ApplicationUserDTO.cs
+++ Entities/DTOs/ApplicationUserDTO.cs
@@ -4,39 +4,58 @@ namespace Entities.DTOs{
 	public class ApplicationUserDTO
 	{
 		[Required(ErrorMessage = "Name is required")]
-		public string Name { get; set; }
+        //[RegularExpression(@"^[a-zA-ZğüşıöçĞÜŞİÖÇ -']+$", ErrorMessage = "Invalid character. Use only letters, spaces, dashes and single quotes.")]   // gerek de yokmuş zaten kendiliğinden her şeyi ekleyebiliyoruz
+        [MaxLength(100, ErrorMessage = "Name cannot be longer than 300 characters")]
+        public string Name { get; set; }
 
-		[Required(ErrorMessage = "Surname is required")]
-		public string Surname { get; set; }
+        [Required(ErrorMessage = "Surname is required")]
+        //[RegularExpression(@"^[a-zA-ZğüşıöçĞÜŞİÖÇ -']+$", ErrorMessage = "Invalid character. Use only letters, spaces, dashes and single quotes.")]     // gerek de yokmuş zaten kendiliğinden her şeyi ekleyebiliyoruz
+        [MaxLength(100, ErrorMessage = "Surname cannot be longer than 300 characters")]
+        public string Surname { get; set; }
 
-		[Required(ErrorMessage = "Phone number is required")]
+
+        [Required(ErrorMessage = "Phone number is required")]
+        [MaxLength(15, ErrorMessage = "Phone number cannot be longer than 15 characters")]
 		public string PhoneNumber { get; set; }
 
         [Required(ErrorMessage = "Age number is required")]
+        [Range(0, 120, ErrorMessage = "Invalid age")]
         public int Age { get; set; }
 
         [Required(ErrorMessage = "Height number is required")]
+        [Range(0, 250, ErrorMessage = "Invalid height")]
         public int Height { get; set; }
 
         [Required(ErrorMessage = "Weight number is required")]
+        [Range(0, 500, ErrorMessage = "Invalid weight")]
         public int Weight { get; set; }
 
+
         [Required(ErrorMessage = "Address is required")]
-		public string Address { get; set; }
+        //[RegularExpression(@"^[a-zA-Z0-9ğüşıöçĞÜŞİÖÇğüşıöçİĞ\s\S\w\W ]*$")].   // gerek de yokmuş zaten kendiliğinden her şeyi ekleyebiliyoruz
+        [MaxLength(200, ErrorMessage = "Address cannot be longer than 200 characters")]
+        public string Address { get; set; }
 
 		[Required(ErrorMessage = "Gender is required")]
 		public string Gender { get; set; }
 
+
 		[Required(ErrorMessage = "Blood type is required")]
 		public string BloodType { get; set; }
 
-		[Required(ErrorMessage = "Email is required")]
-		[EmailAddress(ErrorMessage = "Invalid email address")]
-		public string Email { get; set; }
+  
+        [Required(ErrorMessage = "Email is required")]                                                          
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        [MaxLength(100, ErrorMessage = "Email cannot be longer than 100 characters")]
+        public string Email { get; set; }
 
-		[Required(ErrorMessage = "Password is required")]
+        
+        [Required(ErrorMessage = "Password is required")]
 		[DataType(DataType.Password)]
-		public string Password { get; set; }
+        //[RegularExpression(@"^(?=.*[a-zğüşıöç])(?=.*[A-ZĞÜŞİÖÇ])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$",         // şimdilik kolaylık olsun diye yorum satırı
+        //ErrorMessage = "Password must have at least 1 lowercase letter, 1 uppercase letter, 1 digit, 1 special character and 8 characters in total.")]
+        public string Password { get; set; }
+
 
 		[Required(ErrorMessage = "Confirm password is required")]
 		[DataType(DataType.Password)]
