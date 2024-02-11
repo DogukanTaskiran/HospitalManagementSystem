36d525a77c801d456554c4cca387613830146855
ecedagistan <ecedagistan00@gmail.com>
2024-02-11 13:30:43 +0300

    departmentList,Home department,department controller

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Entities.Models;
+using System.Linq;
+
+namespace Hospital.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DepartmentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult DepartmentList()
+        {
+            var departments = _context.departments.ToList(); // 'departments' küçük harfle başlıyor
+            return View(departments);
+        }
+    }
+}
